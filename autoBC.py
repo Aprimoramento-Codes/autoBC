e9d0c1fc5505da42c535d5f074e2b5bc2e3c57e9
Arthur de Souza Molina <arthur.souza.molina@gmail.com>
2022-07-10 22:57:29 -0300

    FInalmente pronto
    Preciso resolver todas as questões que levantei. Porém resolvi o problema das versões do Chrome e drive de automação. A automação ocorre no agendador de tarefas

--- autoBC.py
+++ autoBC.py
@@ -1,75 +1,52 @@
+from os.path import isfile, realpath, dirname
+from turtle import delay
+from selenium import webdriver    
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 from subprocess import run
-from platform import system
-from sys import modules
-from time  import sleep
-from os.path import isdir, isfile, realpath, dirname
+from datetime import date
 
-#request(plataform)  <-------------------------------------------
-plataform = system()
+clear = lambda: run('cls', shell=True)
+
 dir = dirname(realpath(__file__))
-print('Sistema identificado: {}'.format(plataform))
 
-def requests():
-    if 'selenium' not in modules:
-<<<<<<< HEAD
-=======
-        
->>>>>>> 2ad84be52ba669447bdd0d881fe9ea3be0e38268
-        print("Dependência não encontrada: Selenium")
-        print("Instalando Selenium ...")
-        run(['python3','-m','pip','install','selenium'])
-        
-    if 'webdriver_manager' not in modules:
-        print("Dependência não encontrada: Webdriver Manager")
-        print("Instalando Webdriver Manager ...")
-        
-        run(['python3','-m','pip','install','webdriver_manager'])
+ok = 'OK'
+mensagem0 = "Dados do Usuario: "
+mensagem1 = "Navegador Automatizado: "
+mensagem2 = "Renovação: "
+mensagem3 = "Consulta: "
+mensagem4 = "Data de renovação: "
+mensagem5 = "Agendamento de renovação: "
 
-if not isfile('user.txt'):
-    requests()
-    print("Não há usuário registrado.\nPreencha com atencao...\n\n")
+mensagens = [mensagem0, mensagem1, mensagem2, mensagem3, mensagem4, mensagem5]
 
-    user = input("Digite o código de barras: ")
-    password = input ("Digite a senha: ")
+def status(num):
+    clear()
+    for i in range(num):
+        print(mensagens[i] + ok)
 
-    file = open ('user.txt','w')
-    file.write (user + '\n' + password+'\n')
-
-    file.close()
-
-if plataform == 'Windows':
-    if not isdir('C:\Program Files\Google\Chrome\Application'):
-            input("Instale o Chrome, há um instalador na pasta 'chromesetup', realize a instalação e me execute novamente.")
-            exit()
-            
-def bot(username, password, renew =True, consult = False):
+def bot(username, password, renew =True, consult = True):
     #Drive de automação do navegador
-    from selenium import webdriver
-<<<<<<< HEAD
-    from selenium.webdriver.chrome.service import Service
-=======
->>>>>>> 2ad84be52ba669447bdd0d881fe9ea3be0e38268
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.common.keys import Keys
-    from webdriver_manager.chrome import ChromeDriverManager
-    ###############################################
-<<<<<<< HEAD
     
+    ###############################################
+
     browser = webdriver.Chrome (service=Service(ChromeDriverManager().install()))
-=======
     options = webdriver.ChromeOptions()
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
     
     browser = webdriver.Chrome (ChromeDriverManager().install(), options= options)
->>>>>>> 2ad84be52ba669447bdd0d881fe9ea3be0e38268
     browser.get("http://virtua.uel.br:8080/auth/login?")
-    
+    status(1)
+    delay(4)
     browser.find_element(By.NAME,"username").send_keys(username)
     browser.find_element(By.NAME,"password").send_keys(password + Keys.RETURN)
     
     if renew:
         browser.find_element(By.NAME,"button.selectAll").click()
         browser.find_element(By.ID,"button.renew").click()
+        status(2)
         
     if consult:
         datas_de_vencimento = []
@@ -82,6 +59,7 @@ def bot(username, password, renew =True, consult = False):
         for i in range(quantidade_de_livros):
             datas_de_vencimento.append(browser.find_element(By.XPATH,"/html/body/div/div[2]/div[5]/div[4]/div[1]/form[1]/table/tbody/tr[{}]/td[4]/div".format(i+1)).text)
     browser.quit()
+    status(3)
     return datas_de_vencimento
 
 def user_file():
@@ -96,21 +74,14 @@ def user_file():
         password = password
         file.close()
 
-<<<<<<< HEAD
+        status(0)
     return bot (user, password, renew=True, consult=True)
-=======
-        bot (user, password, renew=False, consult=True)
->>>>>>> 2ad84be52ba669447bdd0d881fe9ea3be0e38268
-
+        
 def find_Due_date(datas):
     anos = [int(data[6:]) for data in datas]
     meses =  [int(data[3:5]) for data in datas]
     dias = [int(data[:2]) for data in datas]
-<<<<<<< HEAD
-
-=======
-    print(anos, meses,dias)
->>>>>>> 2ad84be52ba669447bdd0d881fe9ea3be0e38268
+    
     if len(set(anos)) == 1:
         if len(set(meses)) == 1:
             if len(set(dias)) == 1:
@@ -129,52 +100,50 @@ def find_Due_date(datas):
         indices_mes = [i for i in range(len(meses)) if meses[i]==min(meses)]
         dias_mes_min = [dias[i] for i in indices_mes]
         data_de_vencimento = [datas[i] for i in range(len(datas)) if dias[i]==min(dias_mes_min)]
-        
-<<<<<<< HEAD
-    with open('renew.txt','w') as file:
-            file.write(data_de_vencimento)
-            file.close()
-        
-=======
->>>>>>> 2ad84be52ba669447bdd0d881fe9ea3be0e38268
+    status(4)
+    print("A proxima execucao sera no dia {}".format(data_de_vencimento))
     return data_de_vencimento
 
 def autoexe(data_de_vencimento):
-    from datetime import date
     if date.today().strftime("%d/%m/%Y") == data_de_vencimento:
-        data_de_vencimento == find_Due_date(user_file())
-        with open('renew.txt','w') as file:
-            file.write(data_de_vencimento)
-            file.close()
+        data_de_vencimento = find_Due_date(user_file())
+        
+        if not isfile('renew.txt'):
+            run(['schtasks','/create','/tn','Renew Books','/tr','python autoBC.py','/sc', 'ONCE', '/sd',data_de_vencimento,'/st', '08:00','/f']) 
+            status(5)
+            print("A proxima execucao sera no dia {}".format(data_de_vencimento))
+            with open('renew.txt','w') as file:
+                file.write(data_de_vencimento)
+                file.close()
+        else:
+            run(['schtasks','/change','/tn','Renew Books','/sd',data_de_vencimento,'/st', '08:00','/f'])
+            status(5)
+            print("A proxima execucao sera no dia {}".format(data_de_vencimento))
     else:
-        print("A proxima execucao sera no dia {}".format(data_de_vencimento))
+        print("A proxima execucao sera no dia {}".format(data_de_vencimento)) 
 
-if isfile(dir+'\\renew.txt'):
+if not isfile('user.txt'):
+    print("Não há usuário registrado.\nPreencha com atencao...\n\n")
+
+    user = input("Digite o código de barras: ")
+    password = input ("Digite a senha: ")
+
+    file = open ('user.txt','w')
+    file.write (user + '\n' + password+'\n')
+
+    file.close()
     
+    datas_de_vencimento =  bot (user, password, renew=False, consult=True)
+    
+    data_de_vencimento = find_Due_date(datas_de_vencimento)
+    
+elif isfile(r"\renew.txt"):  
     with open('renew.txt','r') as file:
         data_de_vencimento = file.readline()
     file.close()
-    
     autoexe(data_de_vencimento)
-<<<<<<< HEAD
-else:
-    autoexe(find_Due_date(user_file()))
-           
-'''
-=======
-
-
-        
->>>>>>> 2ad84be52ba669447bdd0d881fe9ea3be0e38268
-if not isfile("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\\autoBC.py") and plataform == 'Windows':
-    Inicializar = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
-    from shutil import copyfile
-    copyfile(dir +'\\autoBC.py',Inicializar)
-    copyfile(dir +'\\user.txt',Inicializar)
-    copyfile(dir + '\\renew.txt',Inicializar)
-<<<<<<< HEAD
-''' 
-=======
     
-input()  
->>>>>>> 2ad84be52ba669447bdd0d881fe9ea3be0e38268
+else:
+    autoexe(date.today().strftime("%d/%m/%Y"))
+    
+input("Pressione qualquer tecla para sair...")
---
+++ boot.bat
@@ -0,0 +1,1 @@
+python "C:\Users\arthu\Desktop\autoBC\autoBC.py" --user
